2983bf171c794f83f447bb407a1c01d54599886d
刘志伟 <liuzhw@91160.com>
2017-08-07 19:28:19 +0800

    说明

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 2、NEC-[NEC : 更好的CSS样式解决方案（网易）](http://nec.netease.com/) 【GitHub:<https://github.com/awei922/html/tree/master/NEC>】  
 3、homePage-[个人主页demo](https://awei922.github.io/html/homePage/index.html)       
 4、cropperjs-demo-[h5利用cropper.js实现图片裁剪demo](https://awei922.github.io/html/cropperjs-demo/index.html)            
-5、swiper-demo-[利用swiper动态创建图片预览效果（兼容移动端）](https://awei922.github.io/html/swiper-demo/index.html)  
+5、swiper-demo-[利用swiper动态创建图片预览效果（兼容移动端）](https://awei922.github.io/html/swiper-demo/index.html)     
+6、audioplayerJs-[webapp音频播放插件audioplay.js](https://awei922.github.io/html/audioplayerJs/index.html) 
--- audioplayerJs/js/audioplay.js
+++ audioplayerJs/js/audioplay.js
@@ -1,102 +1,104 @@
-var audioPlay = function(el){
-    that = this;
-    that.el = document.querySelector(el);
-    that.audio = document.querySelector('audio');
+var AudioPlay = function(el){
+    this.el = document.querySelector(el);
+    this.audio = document.querySelector('audio');
 
     //初始化
-    that.playbtn = that.el.querySelector('.audioplay-play');
-    that.audioplaybar = that.el.querySelector('.audioplay-bar');
-    that.progress = that.el.querySelector('.audioplay-progress');
-    that.playhead = that.el.querySelector('.audioplay-playhead');
-    that.audio.src = that.el.querySelector('.audioplay-play').getAttribute('data-src');
-    that.timeLine = that.audioplaybar.offsetWidth;
-     that.positionX = 0;
+    this.playbtn = this.el.querySelector('.audioplay-play');
+    this.audioplaybar = this.el.querySelector('.audioplay-bar');
+    this.progress = this.el.querySelector('.audioplay-progress');
+    this.playhead = this.el.querySelector('.audioplay-playhead');
+    this.audio.src = this.el.querySelector('.audioplay-play').getAttribute('data-src');
+    this.timeLine = this.audioplaybar.offsetWidth;
+    this.positionX = 0;
+};
+AudioPlay.prototype = {
+    init:function(){
+        this.bindEvent();      
+    },
+    bindEvent:function(){
+        var that = this;
+        this.playbtn.addEventListener('click',that.play.bind(this));           
+        //更新进度条
+        this.audio.addEventListener('timeupdate',that.timeupdate.bind(this));
+        this.audio.addEventListener('ended',function(){that.removeClass(that.playbtn,'active');});
 
-    that.playbtn.addEventListener('click',play);   
-    
-    //更新进度条
-    that.audio.addEventListener('timeupdate',timeupdate);
-    that.audio.addEventListener('ended',function(){removeClass(that.playbtn,'active');});
-
-    //滑动进度条
-    that.playhead.addEventListener('touchstart',touchstart);
-    that.playhead.addEventListener('touchmove',touchmove);
-    that.playhead.addEventListener('touchend',touchend);
-    
-    function touchstart(e){
-        removeClass(that.playbtn,'active');
-        that.audio.pause();
-        that.startX = e.changedTouches[0].pageX; 
-    }
-    
-    function touchmove(e){
-        that.moveX = that.positionX + e.changedTouches[0].pageX-that.startX;                             
-        that.moveX = that.moveX>=timeLine ? that.timeLine : that.moveX<=0 ? 0 : that.moveX;
-
-        that.playhead.style.left = that.moveX/that.timeLine*100+'%';
-        that.progress.style.width = that.moveX/that.timeLine*100+'%';
-        that.audio.currentTime = that.moveX/that.timeLine*that.audio.duration;  
-    }
-    function touchend(e){
-        that.positionX = that.positionX + e.changedTouches[0].pageX-that.startX;
-        that.positionX = that.positionX>=that.timeLine ? 0 : that.positionX<=0 ? 0 : that.positionX;
-        
-        console.log(that.positionX);
-        //处理播放时间                
-        that.audio.currentTime = that.positionX/that.timeLine*that.audio.duration; 
-
-        //滑动完成后播放
-        addClass(that.playbtn,'active');
-        that.audio.play();
-    }
-
-    function timeupdate(){
-        var percent = that.audio.currentTime/that.audio.duration;
-      
-        that.playhead.style.left=percent*100+'%';
-        that.progress.style.width=percent*100+'%'
-    }
-  
-    function play(){      
-        if(that.audio.paused){
-            if(that.audio.currentTime>0 || that.audio.readyState>1){
-                addClass(that.playbtn,'active');
-                that.audio.play(); 
+        //滑动进度条
+        this.playhead.addEventListener('touchstart',that.touchstart.bind(this));
+        this.playhead.addEventListener('touchmove',that.touchmove.bind(this));
+        this.playhead.addEventListener('touchend',that.touchend.bind(this));
+    },
+    play:function(){     
+        var that = this; 
+        if(this.audio.paused){
+            if(this.audio.currentTime>0 || this.audio.readyState>1){
+                this.addClass(this.playbtn,'active');
+                this.audio.play(); 
             }else{
-                addClass(that.playbtn,'loading');
-                that.audio.load();  
-                that.audio.addEventListener("loadedmetadata",function() {
-                    removeClass(that.playbtn,'loading'); 
-                    addClass(that.playbtn,'active');
+                this.addClass(this.playbtn,'loading');
+                this.audio.load();  
+                this.audio.addEventListener("loadedmetadata",function() {
+                    that.removeClass(that.playbtn,'loading'); 
+                    that.addClass(that.playbtn,'active');
                     that.audio.play();                                   
                     that.positionX = that.audio.currentTime/that.audio.duration*that.timeLine;
                 }); 
             }
         }else{
-            removeClass(that.playbtn,'active');
-            that.audio.pause();
+            this.removeClass(this.playbtn,'active');
+            this.audio.pause();
         }
-    }   
+    },
+    touchstart:function(e){
+        this.removeClass(this.playbtn,'active');
+        this.audio.pause();
+        this.startX = e.changedTouches[0].pageX; 
+    },
+    touchmove:function(e){
+        this.moveX = this.positionX + e.changedTouches[0].pageX-this.startX;                             
+        this.moveX = this.moveX>=this.timeLine ? this.timeLine : this.moveX<=0 ? 0 : this.moveX;
 
-    function hasClass (elem, cls) {
-        cls = cls || '';
-        if (cls.replace(/\s/g, '').length == 0) return false; //当cls没有参数时，返回false
-        return new RegExp(' ' + cls + ' ').test(' ' + elem.className + ' ');
-    }
-    
-    function addClass(elem, cls) {
-        if (!hasClass(elem, cls)) {
+        this.playhead.style.left = this.moveX/this.timeLine*100+'%';
+        this.progress.style.width = this.moveX/this.timeLine*100+'%';
+        this.audio.currentTime = this.moveX/this.timeLine*this.audio.duration;  
+    },
+    touchend:function(e){
+        this.positionX = this.positionX + e.changedTouches[0].pageX-this.startX;
+        this.positionX = this.positionX>=this.timeLine ? 0 : this.positionX<=0 ? 0 : this.positionX;
+        
+        console.log(this.positionX);
+        //处理播放时间                
+        this.audio.currentTime = this.positionX/this.timeLine*this.audio.duration; 
+
+        //滑动完成后播放
+        this.addClass(this.playbtn,'active');
+        this.audio.play();
+    },
+    addClass:function(elem, cls) {
+        if (!this.hasClass(elem, cls)) {
             elem.className = elem.className == '' ? cls : elem.className + ' ' + cls;
         }
-    }
-    
-    function removeClass(elem, cls) {
-        if (hasClass(elem, cls)) {
+    },
+    removeClass:function(elem, cls) {
+        if (this.hasClass(elem, cls)) {
             var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, '') + ' ';
             while (newClass.indexOf(' ' + cls + ' ') >= 0) {
             newClass = newClass.replace(' ' + cls + ' ', ' ');
             }
             elem.className = newClass.replace(/^\s+|\s+$/g, '');
         }
+    },
+
+    hasClass:function (elem, cls) {
+        cls = cls || '';
+        if (cls.replace(/\s/g, '').length == 0) return false; //当cls没有参数时，返回false
+        return new RegExp(' ' + cls + ' ').test(' ' + elem.className + ' ');
+    },
+    timeupdate:function(){
+        var percent = this.audio.currentTime/this.audio.duration;
+      
+        this.playhead.style.left=percent*100+'%';
+        this.progress.style.width=percent*100+'%'
     }
-};
+
+}
+// export default AudioPlay;
